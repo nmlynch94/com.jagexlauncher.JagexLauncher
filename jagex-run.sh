4d738bf919093dc76fc45338c6c6356a91f6f8a5
nate <nmlynch94@gmail.com>
2023-11-17 00:24:08 -0500

    test

--- jagex-run.sh
+++ jagex-run.sh
@@ -12,11 +12,21 @@ if [ ! -d "$wineprefix" ]; then
     mkdir -p "$wineprefix/drive_c/users/$(whoami)/AppData/Local/Jagex Launcher"
     cp /app/launcher-win.production.json "$wineprefix/drive_c/users/$(whoami)/AppData/Local/Jagex Launcher/"
 
+    mkdir -p "$wineprefix/drive_c/Program Files (x86)/Jagex Launcher"
+    cp /app/*.dll "$wineprefix/drive_c/Program Files (x86)/Jagex Launcher/"
+    cp /app/*.dat "$wineprefix/drive_c/Program Files (x86)/Jagex Launcher/"
+    cp /app/*.pak "$wineprefix/drive_c/Program Files (x86)/Jagex Launcher/"
+    cp /app/*.exe "$wineprefix/drive_c/Program Files (x86)/Jagex Launcher/"
+    cp /app/*.bin "$wineprefix/drive_c/Program Files (x86)/Jagex Launcher/"
+    cp /app/*.json "$wineprefix/drive_c/Program Files (x86)/Jagex Launcher/"
+    cp /app/*.jar "$wineprefix/drive_c/Program Files (x86)/Jagex Launcher/"
+    cp /app/locales "$wineprefix/drive_c/Program Files (x86)/Jagex Launcher/"
+
     # Make sure the registry has the installation location for runelite.
-    WINEPREFIX="$wineprefix" WINEDEBUG="-all" "$winebin/wine64" reg.exe add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Uninstall\RuneLite Launcher_is1" /v "InstallLocation" /t REG_SZ /d "Z:\app" /f
+    WINEPREFIX="$wineprefix" WINEDEBUG="-all" "$winebin/wine64" reg.exe add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Uninstall\RuneLite Launcher_is1" /v "InstallLocation" /t REG_SZ /d "C:\Program Files (x86)\Jagex Launcher" /f
 
     # Make sure the registry has the installation location for hdos
-    WINEPREFIX="$wineprefix" WINEDEBUG="-all" "$winebin/wine64" reg.exe add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Uninstall\HDOS Launcher_is1" /v "InstallLocation" /t REG_SZ /d "Z:\app" /f
+    WINEPREFIX="$wineprefix" WINEDEBUG="-all" "$winebin/wine64" reg.exe add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Uninstall\HDOS Launcher_is1" /v "InstallLocation" /t REG_SZ /d "C:\Program Files (x86)\Jagex Launcher" /f
 fi
 
-WINEPREFIX="$wineprefix" "$winebin/wine64" /app/JagexLauncher.exe
+WINEPREFIX="$wineprefix" "$winebin/wine64" "$wineprefix/drive_c/Program Files (x86)/Jagex Launcher/JagexLauncher.exe"
