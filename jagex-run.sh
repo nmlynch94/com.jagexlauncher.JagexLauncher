5090e62e1dcd00e64c8f44d6f0c7f3363caee61d
nate <nmlynch94@gmail.com>
2023-11-18 14:12:51 -0500

    Using wine-ge again

--- jagex-run.sh
+++ jagex-run.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 set -x
 # winebin="/app/opt/lutris-GE-Proton8-22-x86_64/bin"
-winebin="/usr/bin"
+winebin="/app/opt/wine/bin"
 wineprefix="$XDG_DATA_HOME"/prefix
+runelite_location="$wineprefix/drive_c/Program Files (x86)/RuneLite"
 
 #  First run setup
 if [ ! -d "$wineprefix" ]; then
@@ -13,10 +14,10 @@ if [ ! -d "$wineprefix" ]; then
     cp /app/launcher-win.production.json "$wineprefix/drive_c/users/$(whoami)/AppData/Local/Jagex Launcher/"
 
     # Make sure the registry has the installation location for runelite.
-    WINEPREFIX="$wineprefix" WINEDEBUG="-all" "wine64" reg.exe add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Uninstall\RuneLite Launcher_is1" /v "InstallLocation" /t REG_SZ /d "Z:\app" /f
+    WINEPREFIX="$wineprefix" WINEDEBUG="-all" "$winebin/wine64" reg.exe add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Uninstall\RuneLite Launcher_is1" /v "InstallLocation" /t REG_SZ /d "Z:\app" /f
 
     # Make sure the registry has the installation location for hdos
-    WINEPREFIX="$wineprefix" WINEDEBUG="-all" "wine64" reg.exe add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Uninstall\HDOS Launcher_is1" /v "InstallLocation" /t REG_SZ /d "Z:\app" /f
+    WINEPREFIX="$wineprefix" WINEDEBUG="-all" "$winebin/wine64" reg.exe add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Uninstall\HDOS Launcher_is1" /v "InstallLocation" /t REG_SZ /d "Z:\app" /f
 fi
 
-WINEPREFIX="$wineprefix" "wine64" /app/JagexLauncher.exe
+WINEPREFIX="$wineprefix" "$winebin/wine64" /app/JagexLauncher.exe
