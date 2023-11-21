4d424b298a46bad109fe36930530c4ca7bbacf5b
nate <nmlynch94@gmail.com>
2023-11-20 20:10:07 -0500

    Fixing finish args

--- jagex-run.sh
+++ jagex-run.sh
@@ -5,7 +5,7 @@ wineprefix="$XDG_DATA_HOME"/prefix
 
 # Make sure metafile is in the proper location. We do this on each boot to make sure it is replaced after an update.
 mkdir -p "$wineprefix/drive_c/users/$(whoami)/AppData/Local/Jagex Launcher"
-cp /app/launcher-win.production.json "$wineprefix/drive_c/users/$(whoami)/AppData/Local/Jagex Launcher/"
+cp /app/extra/metafile.json "$wineprefix/drive_c/users/$(whoami)/AppData/Local/Jagex Launcher/launcher-win.production.json"
 
 # Make sure the registry has the installation location for runelite.
 WINEPREFIX="$wineprefix" WINEDEBUG="-all" "$winebin/wine" reg.exe add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Uninstall\RuneLite Launcher_is1" /v "InstallLocation" /t REG_SZ /d "Z:\app" /f
