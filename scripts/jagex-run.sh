0e762e24620ebf5c503dad199258414fa85731a9
nathanlynch <nathan.lynch@parivedasolutions.com>
2023-11-21 13:42:21 -0500

    Revert "Update jagex-run.sh"
    This reverts commit b10bb09ee6ba96c3e473ef82499472bc4564648c.

--- scripts/jagex-run.sh
+++ scripts/jagex-run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+winebin="/app/opt/wine/bin"
 wineprefix="$XDG_DATA_HOME"/prefix
 
 # Make sure metafile is in the proper location. We do this on each boot to make sure it is replaced after an update.
@@ -6,14 +7,14 @@ mkdir -p "$wineprefix/drive_c/users/$(whoami)/AppData/Local/Jagex Launcher"
 cp /app/extra/metafile.json "$wineprefix/drive_c/users/$(whoami)/AppData/Local/Jagex Launcher/launcher-win.production.json"
 
 # Make sure the registry has the installation location for runelite.
-WINEPREFIX="$wineprefix" WINEDEBUG="-all" "wine" reg.exe add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Uninstall\RuneLite Launcher_is1" /v "InstallLocation" /t REG_SZ /d "Z:\app" /f
+WINEPREFIX="$wineprefix" WINEDEBUG="-all" "$winebin/wine" reg.exe add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Uninstall\RuneLite Launcher_is1" /v "InstallLocation" /t REG_SZ /d "Z:\app" /f
 
 # Make sure the registry has the installation location for hdos
-WINEPREFIX="$wineprefix" WINEDEBUG="-all" "wine" reg.exe add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Uninstall\HDOS Launcher_is1" /v "InstallLocation" /t REG_SZ /d "Z:\app" /f
+WINEPREFIX="$wineprefix" WINEDEBUG="-all" "$winebin/wine" reg.exe add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Uninstall\HDOS Launcher_is1" /v "InstallLocation" /t REG_SZ /d "Z:\app" /f
 
 # Make sure dxvk is in place in our prefix
 cp -r /app/opt/dxvk/x64/*.dll "$wineprefix/drive_c/windows/system32/"
 cp -r /app/opt/dxvk/x32/*.dll "$wineprefix/drive_c/windows/syswow64/"
 
 # Run with overrides for dxvk
-WINEPREFIX="$wineprefix" DXVK_HUD=1 WINEDLLOVERRIDES="d3d11=n;d3d10core=n;dxgi=n;d3d9=n" "wine" /app/extra/JagexLauncher.exe
+WINEPREFIX="$wineprefix" DXVK_HUD=1 WINEDLLOVERRIDES="d3d11=n;d3d10core=n;dxgi=n;d3d9=n" "$winebin/wine" /app/extra/JagexLauncher.exe
