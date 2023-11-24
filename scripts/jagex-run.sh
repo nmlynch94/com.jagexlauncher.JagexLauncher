be9b26524d095df524217a86d393fb1b2d419c11
nmlynch94 <nmlynch94@gmail.com>
2023-11-23 20:26:45 -0500

    Moving files to xdg_data_home

--- scripts/jagex-run.sh
+++ scripts/jagex-run.sh
@@ -6,6 +6,9 @@ wineprefix="$XDG_DATA_HOME"/prefix
 mkdir -p "$wineprefix/drive_c/users/$(whoami)/AppData/Local/Jagex Launcher"
 cp /app/extra/metafile.json "$wineprefix/drive_c/users/$(whoami)/AppData/Local/Jagex Launcher/launcher-win.production.json"
 
+mkdir -p "$wineprefix/drive_c/Program Files (x86)/Jagex Launcher"
+cp -r /app/extra/* "$wineprefix/drive_c/Program Files (x86)/Jagex Launcher/"
+
 # Make sure the registry has the installation location for runelite.
 WINEPREFIX="$wineprefix" WINEDEBUG="-all" "$winebin/wine" reg.exe add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Uninstall\RuneLite Launcher_is1" /v "InstallLocation" /t REG_SZ /d "Z:\app" /f
 
@@ -17,4 +20,4 @@ cp -r /app/opt/dxvk/x64/*.dll "$wineprefix/drive_c/windows/system32/"
 cp -r /app/opt/dxvk/x32/*.dll "$wineprefix/drive_c/windows/syswow64/"
 
 # Run with overrides for dxvk
-WINEPREFIX="$wineprefix" WINEDLLOVERRIDES="d3d11=n;d3d10core=n;dxgi=n;d3d9=n" "$winebin/wine" /app/extra/JagexLauncher.exe
+WINEPREFIX="$wineprefix" WINEDLLOVERRIDES="d3d11=n;d3d10core=n;dxgi=n;d3d9=n" "$winebin/wine" "$wineprefix/drive_c/Program Files (x86)/Jagex Launcher/JagexLauncher.exe"
