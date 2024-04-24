c399b50954c2780823cf9d8977f6cdbf723d1ddc
nate <nmlynch94@gmail.com>
2024-04-23 22:24:51 -0400

    ci: supporting installation using a remote repo instead of bundles

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ cd icons
 cd ..
 
 flatpak remote-add --if-not-exists --user flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak-builder --install-deps-from=flathub --user --install --force-clean build-dir --disable-cache "$(basename $(git rev-parse --show-toplevel)).yml"
+flatpak-builder --repo ./repo --gpg-sign=787C55D80BC4CB5BB385FC67B9D5939A8F40EB05 --user --install --force-clean build-dir --disable-cache "$(basename $(git rev-parse --show-toplevel)).yml"
