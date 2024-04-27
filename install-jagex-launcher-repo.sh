5c574f54c31e9d8f0456a0d57b3e538c1fca79b1
nmlynch94 <40608755+nmlynch94@users.noreply.github.com>
2024-04-27 11:49:15 -0400

    fix: add installation of 32 bit libraries to installation script to work around those dependencies not installing from flathub

--- install-jagex-launcher-repo.sh
+++ install-jagex-launcher-repo.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+FREEDESKTOP_VERSION="23.08"
+
 flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak remote-add --user --if-not-exists JLauncher https://nmlynch94.github.io/com.jagexlauncher.JagexLauncher/JagexLauncher.flatpakrepo
+
+# https://github.com/flatpak/flatpak/issues/3094
+flatpak install --user -y --noninteractive flathub \
+    org.freedesktop.Platform//${FREEDESKTOP_VERSION} \
+    org.freedesktop.Platform.Compat.i386/x86_64/${FREEDESKTOP_VERSION} \
+    org.freedesktop.Platform.GL32.default/x86_64/${FREEDESKTOP_VERSION}
+
 flatpak install -y --user --noninteractive JLauncher com.jagexlauncher.JagexLauncher
