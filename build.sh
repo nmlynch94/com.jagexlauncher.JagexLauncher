e36e7aabe2013a05453653a7d262fe3c19ff93ae
nmlynch94 <40608755+nmlynch94@users.noreply.github.com>
2026-01-11 15:36:49 -0500

    Refactor flatpak-builder command in build.sh

--- build.sh
+++ build.sh
@@ -6,5 +6,4 @@ cd icons
 cd ..
 
 flatpak remote-add --if-not-exists --user flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak-builder --repo=out --default-branch=stable --gpg-sign=FFB01AD591778A1B0321C2E6C38BBBC648E22A4B --require-changes --rebuild-on-sdk-change --install --install-deps-from=flathub --user --ccache --force-clean build-dir com.jagexlauncher.JagexLauncher.yml
-flatpak build-update-repo --gpg-sign=FFB01AD591778A1B0321C2E6C38BBBC648E22A4B out --title="Jagex Launcher" --generate-static-deltas --default-branch=stable --prune
+flatpak-builder --install-deps-from=flathub --user --install --force-clean build-dir --disable-cache "$(basename $(git rev-parse --show-toplevel)).yml"
